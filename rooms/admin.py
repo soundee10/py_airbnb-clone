2135cf28de358db27a971eef234b691abbe1ac05
unknown <malice@korea.ac.kr>
2021-10-14 00:56:15 +0900

    room models ready

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,11 +2,25 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.RoomType)
-class ItemAdmin(admin.ModelAdmin):
+@admin.register(models.Room)
+class RoomAdmin(admin.ModelAdmin):
+
+    """Room Admin Definition"""
+
     pass
 
 
-@admin.register(models.Room)
+@admin.register(models.HouseRule, models.Amenity, models.Facility, models.RoomType)
 class RoomAdmin(admin.ModelAdmin):
+
+    """Item Admin Definition"""
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """Photo Admin Definition"""
+
     pass
--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
-from users import models as user_models
 
 # Create your models here.
 
@@ -19,7 +18,47 @@ class AbstractItem(core_models.TimeStampedModel):
 
 
 class RoomType(AbstractItem):
-    pass
+
+    """Roomtype Model definition"""
+
+    class Meta:
+        verbose_name = "Room Type"
+
+
+class Amenity(AbstractItem):
+
+    """Amenity Model definition"""
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+
+    """Amenity Model definition"""
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+
+    """House Model definition"""
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """Photo Model Definition"""
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class Room(core_models.TimeStampedModel):
@@ -39,8 +78,13 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ManyToManyField(RoomType, blank=True)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey(
+        "RoomType", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rule = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
         return self.name
