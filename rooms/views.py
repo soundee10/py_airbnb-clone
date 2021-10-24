351ddba5fd5292c4fbeb5f4b7a2b60c5da43c42c
unknown <malice@korea.ac.kr>
2021-10-25 01:20:03 +0900

    introduction to views and urls, paginating manually & using django paginator

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.core.paginator import EmptyPage, Paginator
+from . import models
 
-# Create your views here.
+
+def all_rooms(request):
+    page = request.GET.get("page", 1)
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, 3)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", context={"page": rooms})
+    except EmptyPage:
+        return redirect("/")
+
+
+""" pagination in manual"""
+"""
+    page = int(page or 1)
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    page_count = ceil(models.Room.objects.count() / page_size)
+    all_rooms = models.Room.objects.all()[offset:limit]
+    return render(
+        request,
+        "rooms/home.html",
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count + 1),
+        },
+    )
+"""
