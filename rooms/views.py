d2c4db2d3449911de6c308d98e1ab4c45f633e88
unknown <malice@korea.ac.kr>
2021-10-31 03:18:02 +0900

    Manually creating a search filter

--- rooms/views.py
+++ rooms/views.py
@@ -43,8 +43,8 @@ def search(request):
     bedrooms = request.GET.get("bedrooms", 0)
     beds = request.GET.get("beds", 0)
     baths = request.GET.get("baths", 0)
-    instant = request.GET.get("instant", False)
-    super_host = request.GET.get("super_host", False)
+    instant = bool(request.GET.get("instant", False))
+    superhost = bool(request.GET.get("superhost", False))
     s_amenities = request.GET.getlist("amenities")
     s_facilities = request.GET.getlist("facilities")
 
@@ -61,7 +61,7 @@ def search(request):
         "s_amenities": s_amenities,
         "s_facilities": s_facilities,
         "instant": instant,
-        "super_host": super_host,
+        "superhost": superhost,
     }
 
     room_types = models.RoomType.objects.all()
@@ -82,10 +82,40 @@ def search(request):
     filter_args["country"] = country
 
     if room_type != 0:
-        filter_args["room_type__pk__exact"] = room_type
+        filter_args["room_type__pk"] = room_type
+
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+
+    if price != 0:
+        filter_args["price__gte"] = price
+
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    if instant is True:
+        filter_args["instant_book"] = True
+
+    if superhost is True:
+        filter_args["host__superhost"] = True
 
     rooms = models.Room.objects.filter(**filter_args)
 
+    if len(s_amenities) > 0:
+        for s_amenity in s_amenities:
+            rooms = rooms.filter(amenities__pk=int(s_amenity))
+            # filter_args["amenities__pk"] = int(s_amenities)
+
+    if len(s_facilities) > 0:
+        for s_amenity in s_facilities:
+            rooms = rooms.filter(facilities__pk=int(s_amenity))
+            # filter_args["facilities__pk"] = int(s_facilities)
     return render(
         request,
         "rooms/search.html",
