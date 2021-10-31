05b3782500fe80dfb26147e7544a4ccb1a1e4b68
unknown <malice@korea.ac.kr>
2021-10-31 21:15:26 +0900

    #14.3 validating password

---
+++ users/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.forms.widgets import PasswordInput
+from . import models
+
+
+class LoginForm(forms.Form):
+
+    email = forms.EmailField()
+    password = forms.CharField(widget=PasswordInput)
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(username=email)
+            return email
+        except models.User.DoesNotExist:
+            raise forms.ValidationError("User does not exist")
+
+    def clean_password(self):
+        print("clean password")
