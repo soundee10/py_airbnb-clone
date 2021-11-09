d25b567d179a2e8e5a589f9b1fe47216cfc1ff2f
unknown <malice@korea.ac.kr>
2021-11-09 22:46:18 +0900

    #20 Displaying room card part 2

--- rooms/views.py
+++ rooms/views.py
@@ -14,7 +14,7 @@ class HomeView(ListView):
     """Home View Definition"""
 
     model = models.Room
-    paginate_by = 10
+    paginate_by = 12
     paginate_orphans = 3
     ordering = "created"
     context_object_name = "rooms"
