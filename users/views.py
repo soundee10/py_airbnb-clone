10636143126771a9b79bc377fee6cb6e6f974794
unknown <malice@korea.ac.kr>
2021-12-08 02:08:44 +0900

    #21 introduction to mixins

--- users/views.py
+++ users/views.py
@@ -9,10 +9,11 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth.forms import UserCreationForm
-from . import forms, models
+from django.contrib.messages.views import SuccessMessageMixin
+from . import forms, models, mixins
 
 
-class LoginView(FormView):
+class LoginView(mixins.LoggedOutOnlyView, FormView):
 
     template_name = "users/login.html"
     form_class = forms.LoginForm
@@ -211,7 +212,8 @@ class UserProfileView(DetailView):
     model = models.User
     context_object_name = "user_obj"
     
-class UpdateProfileView(UpdateView):
+class UpdateProfileView(SuccessMessageMixin, UpdateView):
+    
     model = models.User
     template_name = "users/update-profile.html"
     fields = (
@@ -225,6 +227,8 @@ class UpdateProfileView(UpdateView):
         "currency",
     )
     
+    success_message = "Profile Updated"
+    
     def get_object(self, queryset=None):
         return self.request.user
     
@@ -234,5 +238,21 @@ class UpdateProfileView(UpdateView):
         self.object.save()
         return super().form_valid(form)
     
-class UpdatePasswordView(PasswordChangeView):
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class=form_class)
+        form.fields["birthdate"].widget.attrs = {"placeholder": "Birthdate"}
+        return form
+class UpdatePasswordView(SuccessMessageMixin, PasswordChangeView):
     template_name = "users/update-password.html"
+    #success_url = revse
+    success_message = "Password changed!"
+    
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class=form_class)
+        form.fields["old_password"].widget.attrs = {"placeholder": "Current password"}
+        form.fields["new_password1"].widget.attrs = {"placeholder": "New password"}
+        form.fields["new_password2"].widget.attrs = {"placeholder": "Confirm new password"}
+        return form
+    
+    def get_success_url(self):
+        return self.request.user.get_absolute_url
